b6f05e1909ac4ff9f0411c8c5a4f877860ef44a9
Riyan-ai-code <parthib100320@gmail.com>
2026-01-29 00:45:55 +0530

    removed automatic and gave static

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
